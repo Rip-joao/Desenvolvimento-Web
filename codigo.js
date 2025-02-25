95e1e286e7cdfde202df40521767ff0097637a0c
Rip-joao <j2897835@gmail.com>
2025-02-24 21:44:25 -0300

    Add files via upload
    aula de segunda

---
+++ codigo.js
@@ -0,0 +1,59 @@
+/*var numero= 7;
+if (numero%2==0){
+    console.log("O número é par");
+}
+else{
+    console.log("O número é ímpar");  
+} 
+/*if (numero%numero==1){
+    console.log("O número não é primo");
+}
+else{
+    console.log("O número é primo");  
+}*/
+/*var y;
+var contador=0;
+for(y=1;y<=numero;y++){
+    if(numero%y==0){
+        contador++;
+    }
+}
+if(contador==2){
+    console.log("O numero é primo")
+}
+else{
+    console.log("O número não é primo")
+}*/
+/*
+var opcao=5;
+var num1=2;
+var num2=0;
+var resultado=0;
+if (opcao==0){
+    /*a opção 0 é soma*/
+/*resultado = num1 + num2; 
+console.log(resultado);
+}
+if (opcao==1){ 
+
+}
+if (opcao==2){
+
+}
+if (opcao==3){
+    
+}
+if (opcao==4){
+    
+}
+else{
+   /* console.log("A opção é inválida.")*/
+
+
+
+var num1 = 0 
+var num2 = 1
+var resultado;
+var quanti;0
+resultado = num1 + num2 
+for(resultado + 1 )
